25c4b0d3ef23799a3299abd5a2ca9cbacdae5cf3
marco-soria <marco.soriaa7@gmail.com>
2024-10-14 15:27:23 -0500

    updated order

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,25 +1,22 @@
-import { prisma } from '@/src/lib/prisma'
-import CategoryIcon from '../ui/CategoryIcon'
-import Logo from '../ui/Logo'
+import prisma from "@/src/lib/prisma";
+import CategoryIcon from "../ui/CategoryIcon";
+import Logo from "../ui/Logo";
 
 async function getCategories() {
-  return await prisma.category.findMany()
+  return await prisma.category.findMany();
 }
 
 export default async function OrderSidebar() {
-  const categories = await getCategories()
-  
+  const categories = await getCategories();
+
   return (
     <aside className="md:w-72 md:h-screen bg-white">
-        <Logo />
-        <nav className='mt-10'>
-            {categories.map(category => (
-              <CategoryIcon 
-                key={category.id}
-                category={category}
-              />
-            ))}
-        </nav>
+      <Logo />
+      <nav className="mt-10">
+        {categories.map((category) => (
+          <CategoryIcon key={category.id} category={category} />
+        ))}
+      </nav>
     </aside>
-  )
+  );
 }
