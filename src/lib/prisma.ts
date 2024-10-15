884f5c1cd186924c7df7f0da53186f8b5b4e807e
marco-soria <marco.soriaa7@gmail.com>
2024-10-15 11:20:05 -0500

    changed prisma config

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -9,18 +9,91 @@
 //     })
 // if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
+//v2
+
+// import { PrismaClient } from "@prisma/client";
+
+// const prismaClientSingleton = () => {
+//   return new PrismaClient();
+// };
+
+// declare const globalThis: {
+//   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+// } & typeof global;
+
+// const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+// export default prisma;
+
+// if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+
+// import { PrismaClient } from "@prisma/client";
+
+// let prisma: PrismaClient;
+
+// if (process.env.NODE_ENV === "production") {
+//   prisma = new PrismaClient();
+// } else {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient();
+//   }
+//   prisma = global.prisma;
+// }
+
+// export default prisma;
+
+// import { PrismaClient } from "@prisma/client";
+
+// const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+
+// export const prisma =
+//   globalForPrisma.prisma ??
+//   new PrismaClient({
+//     log: ["query"],
+//     datasources: {
+//       db: {
+//         url: process.env.DATABASE_URL + "?connection_limit=4",
+//       },
+//     },
+//   });
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+// import { PrismaClient } from "@prisma/client";
+
+// const prisma = new PrismaClient({
+//   log: ["query"],
+//   datasources: {
+//     db: {
+//       url: process.env.DATABASE_URL,
+//       pool: {
+//         max: 10, // Ajusta este valor según tu límite
+//       },
+//     },
+//   },
+// });
+
+// export default prisma;
+
 import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ["query"],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+if (process.env.NODE_ENV === "development") {
+  global.prisma = prisma;
+}
 
 export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
